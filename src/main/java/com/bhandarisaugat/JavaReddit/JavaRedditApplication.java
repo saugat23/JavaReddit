18f3a39a81ea75efd3bf8195c74ed55a77403fb8
saugat23 <saugatbhandari8@gmail.com>
2025-05-05 14:42:00 +0545

    initial

---
+++ src/main/java/com/bhandarisaugat/JavaReddit/JavaRedditApplication.java
@@ -0,0 +1,13 @@
+package com.bhandarisaugat.JavaReddit;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaRedditApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JavaRedditApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/bhandarisaugat/JavaReddit/JavaRedditApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bhandarisaugat.JavaReddit;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavaRedditApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
